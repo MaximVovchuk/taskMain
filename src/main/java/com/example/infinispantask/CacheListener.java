7ddbf7f6d456560ebb6242a540f6bdba0be13726
Maxim Vovchuk <Maxim.Vovchuk@fivesysdev.com>
2023-12-01 15:51:18 +0200

    Misha`s TODO fixed

--- src/main/java/com/example/infinispantask/CacheListener.java
+++ src/main/java/com/example/infinispantask/CacheListener.java
@@ -3,9 +3,7 @@ package com.example.infinispantask;
 import com.example.infinispantask.entities.Department;
 import com.example.infinispantask.ws.ChatWebsocketEndpoint;
 import org.infinispan.notifications.Listener;
-import org.infinispan.notifications.cachelistener.annotation.CacheEntryCreated;
 import org.infinispan.notifications.cachelistener.annotation.CacheEntryModified;
-import org.infinispan.notifications.cachelistener.event.CacheEntryCreatedEvent;
 import org.infinispan.notifications.cachelistener.event.CacheEntryModifiedEvent;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -19,17 +17,11 @@ public class CacheListener {
         this.chatWebsocketEndpoint = chatWebsocketEndpoint;
     }
 
-
-    //TODO listen entries only with specified, not all
     @CacheEntryModified
-    public void modified(CacheEntryModifiedEvent<Long,Department> event) {
-       System.out.println("Cache entry modified: " + event.getNewValue().toString());
-       chatWebsocketEndpoint.sendUpdate("Modified info: "+event.getNewValue().toString());
-    }
-
-    @CacheEntryCreated
-    public void created(CacheEntryCreatedEvent<Long, Department> event) {
-        System.out.println("Cache entry created: " + event.getValue().toString());
-        chatWebsocketEndpoint.sendUpdate("New info: " + event.getValue().toString());
+    public void modified(CacheEntryModifiedEvent<Long, Department> event) {
+        if (event.getNewValue().getId() == 2L) {
+            System.out.println("Cache entry modified: " + event.getNewValue().toString());
+            chatWebsocketEndpoint.sendUpdate("Modified info: " + event.getNewValue().toString());
+        }
     }
 }
